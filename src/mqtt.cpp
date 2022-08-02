93a6c3f456a60516a43bcd4ad9828714ba39f427
Gitau01Frank <franciswainainag@gmail.com>
2022-08-02 15:12:21 +0300

    "mqtt publish-subscribe"

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -21,5 +21,56 @@ bool runMQTT(String command,String payload){
      * @brief 
      * Command paramete containers topic to use
      * Payload parameter ... self explanatory
-     */
+ */
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+}
+
+void reconnect() {
+    //loop until we're reconnected
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        //Create a random client ID
+        String clientID = "ESP32Client-";
+        clientID += String(random(0xffff), HEX);
+        //Attempt to connect
+        if (client.connect(clientID.c_str())) {
+            Serial.println("connected");
+            //once connected, publish an announcement...
+            client.publish("outTopic", "payload");
+            // ... and resubscribe
+            client.subscribe("inTopic");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println("try again in 5 seconds");
+            //wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+
+}
+
+void loop () {
+    if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "payload #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
 }
